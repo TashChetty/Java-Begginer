59653c88973815b9a6b72b77793ad185edb2338c
TashChetty <natasha.chetty2@gmail.com>
2024-01-22 15:32:54 +0200

    Lesson 3

---
+++ Lesson 3/src/ex3_1/exercise/Hello.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex3_1.exercise;
+
+/**
+ *
+ * @author ChettyNa
+ */
+public class Hello {
+    //main shortcut -- type main then ctrl and space
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ Lesson 3/src/ex3_2/exercise/ShoppingCart.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.ShoppingCart to edit this template
+ */
+package ex3_2.exercise;
+
+/**
+ *
+ * @author ChettyNa
+ */
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the shopping cart");
+    }
+}

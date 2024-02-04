aca7569f8c1554c0f18ff1be9a2b56cb7beefec0
TashChetty <natasha.chetty2@gmail.com>
2024-02-04 19:05:14 +0200

    Soccer App

---
+++ Soccer App/14-Exceptions_Practice1/src/utility/PlayerDatabaseException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utility;
+
+/**
+ *
+ * @author ChettyNa
+ */
+public class PlayerDatabaseException extends Exception{
+    
+    public PlayerDatabaseException(String message){
+        super(message);
+    }
+    
+}
+

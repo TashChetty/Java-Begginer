a67801933e27de2e61265a79df6c3f00fd95acdb
TashChetty <natasha.chetty2@gmail.com>
2024-02-27 11:32:47 +0200

    Java Beginner Dummies

--- JavaBeginnerDummies/src/Book3/chapter8/PackageTest.java
+++ JavaBeginnerDummies/src/Book3/chapter8/PackageTest.java
@@ -1,13 +1,14 @@
 package Book3.chapter8;
 
 import com.gmail.markdrinkwater.util.Console;
+import java.io.*;
+import java.awt.*;
+import javafx.controls;
+import javafx.fxml;
 
-public class PackageTest
-{
-    public static void main(String[] args)
-    {
-        while (Console.askYorN("Keep going?"))
-        {
+public class PackageTest {
+    public static void main(String[] args) {
+        while (Console.askYorN("Keep going?")) {
             System.out.println("D'oh!");
         }
     }
